c0615b1418013f5ec413ad67c6f27de09d3ebef7
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 02:46:41 -0300

    Add preview button

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, ImageBackground, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import commonStyles from '../commonStyles';
 import todayImage from '../../assets/imgs/today.jpg';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
@@ -11,6 +21,7 @@ import Task from '../components/Task';
 
 export default class TaskList extends Component {
   state = {
+    showDoneTasks: true,
     tasks: [
       {
         id: Math.random(),
@@ -26,6 +37,10 @@ export default class TaskList extends Component {
       },
     ],
   };
+
+  toggleFilter = () => {
+    this.setState({showDoneTasks: !this.state.showDoneTasks});
+  }
 
   toggleTask = (taskId) => {
     const tasks = [...this.state.tasks];
@@ -43,6 +58,15 @@ export default class TaskList extends Component {
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.iconBar}>
+            <TouchableOpacity onPress={this.toggleFilter}>
+              <Icon
+                name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                size={20}
+                color={commonStyles.colors.secunday}
+              />
+            </TouchableOpacity>
+          </View>
           <View style={styles.titleBar}>
             <Text style={styles.title}>Hoje</Text>
             <Text style={styles.subtitle}>{today}</Text>
@@ -90,4 +114,7 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginBottom: 20,
   },
+  iconBar: {
+    flexDirection: 'row',
+  }
 });
