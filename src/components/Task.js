7fd18d62c5b38d608d74e090a91dced8948f4575
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 02:21:36 -0300

    Add formatted date

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 import commonStyles from '../commonStyles';
 
@@ -9,12 +11,15 @@ export default (props) => {
   const doneOrNotStyle =
     props.doneAt != null ? {textDecorationLine: 'line-through'} : {};
 
+  const date = props.doneAt ? props.doneAt : props.estimateAt;
+  const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM');
+
   return (
     <View style={styles.container}>
       <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
       <View>
         <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-        <Text style={styles.date}>{props.estimateAt + ''}</Text>
+        <Text style={styles.date}>{formattedDate}</Text>
       </View>
     </View>
   );
