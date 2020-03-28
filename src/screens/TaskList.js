654a996ed1340b169b2c75a1f73802c8f3e16c14
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 02:30:42 -0300

    Adds FlatList

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import {View, Text, ImageBackground, StyleSheet, FlatList} from 'react-native';
 
 import commonStyles from '../commonStyles';
 import todayImage from '../../assets/imgs/today.jpg';
@@ -10,6 +10,22 @@ import 'moment/locale/pt-br';
 import Task from '../components/Task';
 
 export default class TaskList extends Component {
+  state = {
+    tasks: [{
+        id: Math.random(),
+        desc: 'Comprar Livro de React Native',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Ler Livro de React Native',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+    ],
+  };
+
   render() {
     const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
     return (
@@ -21,12 +37,11 @@ export default class TaskList extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Task
-            desc="Comprar Livro"
-            estimateAt={new Date()}
-            doneAt={new Date()}
+          <FlatList
+            data={this.state.tasks}
+            keyExtractor={(item) => `${item.id}`}
+            renderItem={({item}) => <Task {...item} />}
           />
-          <Task desc="Ler Livro" estimateAt={new Date()} doneAt={null} />
         </View>
       </View>
     );
