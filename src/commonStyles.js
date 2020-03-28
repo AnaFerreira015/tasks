25feae11e7ae4480b37dbcc97f022e1c06050621
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 03:28:36 -0300

    Creates the area for the form

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,6 +1,7 @@
 export default {
   fontFamily: 'Lato',
   colors: {
+    today: '#B13B44',
     secunday: '#FFF',
     mainText: '#222',
     subText: '#555',
--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
-import {Modal, View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {
+  Modal,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native';
+
+import commonStyles from '../commonStyles';
 
 export default class AddTask extends Component {
   render() {
@@ -12,6 +20,14 @@ export default class AddTask extends Component {
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
           <View style={styles.background} />
         </TouchableWithoutFeedback>
+
+        <View style={styles.container}>
+          <Text style={styles.header}>Nova Tarefa</Text>
+        </View>
+
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
       </Modal>
     );
   }
@@ -21,5 +37,17 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today,
+    color: commonStyles.colors.secunday,
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 18,
   },
 });
