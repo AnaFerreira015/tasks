127e60b6dd230f3250eb67f095b75af074779747
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 03:20:06 -0300

    Adds transparency modal to the application

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -18,10 +18,12 @@ import moment from 'moment';
 import 'moment/locale/pt-br';
 
 import Task from '../components/Task';
+import AddTask from './AddTask';
 
 export default class TaskList extends Component {
   state = {
     showDoneTasks: true,
+    showAddTask: true,
     visibleTasks: [],
     tasks: [
       {
@@ -74,6 +76,10 @@ export default class TaskList extends Component {
     const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
     return (
       <View style={styles.container}>
+        <AddTask
+          isVisible={this.state.showAddTask}
+          onCancel={() => this.setState({showAddTask: false})}
+        />
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.iconBar}>
             <TouchableOpacity onPress={this.toggleFilter}>
