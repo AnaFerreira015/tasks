f782a4c29f1344886623e492c923545c21a8f8be
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-03-28 01:51:07 -0300

    Creates Task component

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default (props) => {
+  return (
+    <View>
+      <Text>{props.desc}</Text>
+      <Text>{props.estimateAt + ''}</Text>
+      <Text>{props.doneAt + ''}</Text>
+    </View>
+  );
+};
