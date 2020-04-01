283a7859d6242828bd2eb6b0be502aace71416f7
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-01 18:37:01 -0300

    Form validation

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -70,6 +70,17 @@ export default class Auth extends Component {
   };
 
   render() {
+    const validations = [];
+    validations.push(this.state.email && this.state.email.includes('@'));
+    validations.push(this.state.password && this.state.password.length >= 6);
+
+    if (this.state.stageNew) {
+      validations.push(this.state.name && this.state.name.trim().length >= 3);
+      validations.push(this.state.password === this.state.confirmPassword);
+    }
+
+    const validForm = validations.reduce((t, a) => t && a);
+
     return (
       <ImageBackground source={backgroundImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
@@ -111,8 +122,9 @@ export default class Auth extends Component {
               onChangeText={confirmPassword => this.setState({confirmPassword})}
             />
           )}
-          <TouchableOpacity onPress={this.signinOrSignup}>
-            <View style={styles.button}>
+          <TouchableOpacity onPress={this.signinOrSignup} disabled={!validForm}>
+            <View
+              style={[styles.button, validForm ? {} : styles.disabledButton]}>
               <Text style={styles.buttonText}>
                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
               </Text>
@@ -168,6 +180,9 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
     borderRadius: 7,
+  },
+  disabledButton: {
+    backgroundColor: '#AAA',
   },
   buttonText: {
     fontFamily: commonStyles.fontFamily,
