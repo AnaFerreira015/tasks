33732c8547f4fa7498237dc1a8889f34a58de6e1
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-01 18:19:39 -0300

    Update IP Server

--- src/common.js
+++ src/common.js
@@ -1,14 +1,14 @@
 import {Alert, Platform} from 'react-native';
 
 const server =
-  Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+  Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://192.168.0.103:3000';
 
 function showError(err) {
   Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`);
 }
 
 function showSuccess(msg) {
-  Alert.alert('Sucess', msg);
+  Alert.alert('Sucesso!', msg);
 }
 
 export {server, showError, showSuccess};
