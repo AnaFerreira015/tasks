4c6aabeb477fdbc7461285b79408bc036442db03
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-01 08:38:33 -0300

    Creates the authentication screen

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import TaskList from './src/screens/TaskList';
+import Auth from './src/screens/Auth';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => TaskList);
+AppRegistry.registerComponent(appName, () => Auth);
