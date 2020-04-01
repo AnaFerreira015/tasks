907c0e07b9660e83a6d5f0bf93c753f84d0a2cdf
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-01 17:28:25 -0300

    Update auth

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,9 +8,13 @@ import {
   Alert,
 } from 'react-native';
 
+import axios from 'axios';
+
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
 import AuthInput from '../components/AuthInput';
+
+import {server, showError, showSucess} from '../commonStyles';
 
 export default class Auth extends Component {
   state = {
@@ -23,9 +27,25 @@ export default class Auth extends Component {
 
   signinOrSignup = () => {
     if (this.state.stageNew) {
-      Alert.alert('Sucesso!', 'Criar conta');
+      this.signup();
     } else {
       Alert.alert('Sucesso!', 'Logar');
+    }
+  };
+
+  signup = async () => {
+    try {
+      await axios.post(`${server}/signup`, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword,
+      });
+
+      showSucess('Usuário cadastrado!');
+      this.setState({stageNew: false});
+    } catch (e) {
+      showError(e);
     }
   };
 
