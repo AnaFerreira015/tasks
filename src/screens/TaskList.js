e22b9209d019e038f618e231e0afa9d9109805fe
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-02 09:18:28 -0300

    Dynamic title

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -136,7 +136,7 @@ export default class TaskList extends Component {
             </TouchableOpacity>
           </View>
           <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.title}>{this.props.title}</Text>
             <Text style={styles.subtitle}>{today}</Text>
           </View>
         </ImageBackground>
