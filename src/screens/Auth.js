620521801b7e34b2c537d5051ec0b5b399ebb98a
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-04-02 14:15:41 -0300

    User email access

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -62,7 +62,7 @@ export default class Auth extends Component {
       });
 
       axios.defaults.headers.common.Authorization = `bearer ${res.data.token}`;
-      this.props.navigation.navigate('Home');
+      this.props.navigation.navigate('Home', res.data);
     } catch (e) {
       showError(e);
     }
